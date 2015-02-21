7c8df55c10877b245b26620f28adccc5154f9987
Cyril Wack <contact@cyril.email>
2015-02-22 00:38:50 +0900

    add a code example

--- lib/matchi.rb
+++ lib/matchi.rb
@@ -5,5 +5,9 @@ end
 # Namespace for the Matchi library.
 #
 # @api public
+#
+# @example Expect 42 to be equal to 42
+#   matcher = Matchi::Equal.new(42)
+#   matcher.matches? { 42 } # => true
 module Matchi
 end
