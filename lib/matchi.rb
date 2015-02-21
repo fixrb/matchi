f8944c83b972006f26a199fa59e4194fc9730d1f
Cyril Wack <contact@cyril.email>
2015-02-22 00:15:21 +0900

    require all matchers in matchi/ directory

--- lib/matchi.rb
+++ lib/matchi.rb
@@ -1,10 +1,6 @@
-require_relative File.join 'matchi', 'eql'
-require_relative File.join 'matchi', 'equal'
-require_relative File.join 'matchi', 'match'
-require_relative File.join 'matchi', 'raise_exception'
-require_relative File.join 'matchi', 'be_true'
-require_relative File.join 'matchi', 'be_false'
-require_relative File.join 'matchi', 'be_nil'
+Dir[File.join File.dirname(__FILE__), 'matchi', '*.rb'].each do |fname|
+  require_relative fname
+end
 
 # Namespace for the Matchi library.
 module Matchi
